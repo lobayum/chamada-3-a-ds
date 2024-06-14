7f157de0260837d7d99fd811760f45b9ec14e413
lobayum <142242330+lobayum@users.noreply.github.com>
2024-06-14 08:22:28 -0300

    Add files via upload

---
+++ chamada.js
@@ -0,0 +1,9 @@
+const alunos = ['Adriano Noviski Gomes Dos Santos', 'Emilly Crisanto da Cruz Freire', 'Emilly Priscila Macedo Viera', 'Felipe De Souza Motta', 'Guilherme De Vito Gebing', 'Henrique Luiz Camilo Pradi', 'Izabela Malluceli', 'Kauã Dyego Prado França', 'Kauan José Viola Marcelino', 'Luana Da Rosa Quadros', 'Livia Bicalho Dos Santos', 'Luan Carlos Moreira', 'Giovana', 'Nicole Gabriele Miranda Lobato', 'Raquel Ester Cruz Pacheco', 'Renato Souza Alves Junior', 'Kauã Moura de Souza', 'Lucas Da Silva Salvador'];
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 13, 14, 16, 17, 18, 19, 21, 22];
+
+nome.splice(12,1, "Maria Helena Dos Santos Mucci Daniel");
+numero.splice(12,1, 16);
+
+for (let i = 0; i < nome.length; i++) {
+    console.log(alunos[i]+ " - " + num[i]);
+}
